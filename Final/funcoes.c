345e80e99411361cbed8513e3f8dc9b853d5a02b
LourencoPonces <lourenco.poncesduarte@gmail.com>
2023-04-17 17:32:33 +0100

    Update in functions

--- Final/funcoes.c
+++ Final/funcoes.c
@@ -47,7 +47,7 @@ int eJogadaValida(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO], int jogad
                         /*if (tabuleiro[r][c] == Empty)
                         {
                             is_valid = 0;
-                        }*/
+                        } ISTO E NECESSARIO*/
                         if (tabuleiro[linhaAdj][colunaAdj] == jogadorAtual && encontrouAdvers)
                         {
                             return 1;
@@ -67,26 +67,10 @@ int eJogadaValida(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO], int jogad
     return 0;
 }
 
-int haJogadaValida(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO], int jogadorAtual)
-{
-    int linha, coluna;
-    for (linha = 0; linha < TAMANHO_TABULEIRO; linha++)
-    {
-        for (coluna = 0; coluna < TAMANHO_TABULEIRO; coluna++)
-        {
-            if (eJogadaValida(tabuleiro, jogadorAtual, linha, coluna))
-            {
-                return 1;
-            }
-        }
-    }
-    return 0;
-}
-
 void imprimirTabuleiro(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO])
 {
     int i, j;
-    printf("  ");
+    printf("   ");
     for (i = 0; i < TAMANHO_TABULEIRO; i++)
     {
         printf("%d ", i);
@@ -94,7 +78,7 @@ void imprimirTabuleiro(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO])
     printf("\n");
     for (i = 0; i < TAMANHO_TABULEIRO; i++)
     {
-        printf("%d ", i);
+        printf(" %d ", i);
         for (j = 0; j < TAMANHO_TABULEIRO; j++)
         {
             switch (tabuleiro[i][j])
@@ -108,64 +92,180 @@ void imprimirTabuleiro(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO])
             case BRANCO:
                 printf("O ");
                 break;
+            case POSSIBILIDADE:
+                printf("P ");
+                break;
             }
         }
         printf("\n");
     }
 }
 
-int avaliarPosicao()
+int haJogadaValida(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO], int jogadorAtual)
 {
-    return 1;
+    int linha, coluna;
+    for (linha = 0; linha < TAMANHO_TABULEIRO; linha++)
+    {
+        for (coluna = 0; coluna < TAMANHO_TABULEIRO; coluna++)
+        {
+            if (eJogadaValida(tabuleiro, jogadorAtual, linha, coluna))
+            {
+                if (jogadorAtual == 1)
+                {
+                    return 1;
+                }
+                else
+                {
+                    tabuleiro[linha][coluna] = POSSIBILIDADE;
+                }
+            }
+        }
+    }
+    return 0;
+}
+
+int count_opponent_pieces(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO], int jogadorAtual, int row, int col, int dr, int dc)
+{
+    int r = row + dr;
+    int c = col + dc;
+    int count = 0;
+    while (r >= 0 && r < TAMANHO_TABULEIRO && c >= 0 && c < TAMANHO_TABULEIRO)
+    {
+        if (tabuleiro[r][c] == jogadorAtual)
+        {
+            return count;
+        }
+        if (tabuleiro[r][c] == VAZIO)
+        {
+            return 0;
+        }
+        count++;
+        r += dr;
+        c += dc;
+    }
+    return 0;
+}
+
+int avaliarPosicao(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO], int jogadorAtual, int row, int col)
+{
+    int value = 0;
+    int dr, dc;
+    for (dr = -1; dr <= 1; dr++)
+    {
+        for (dc = -1; dc <= 1; dc++)
+        {
+            if (dr == 0 && dc == 0)
+            {
+                continue;
+            }
+            value += count_opponent_pieces(tabuleiro, jogadorAtual, row, col, dr, dc);
+        }
+    }
+    return value;
 }
 
 void undo()
 {
 }
 
+int mudarJogador(int jogadorAtual)
+{
+    if (jogadorAtual == 1)
+        return 0;
+    else
+        return 1;
+}
+
 void fazerJogada(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO], int jogadorAtual, int linha, int coluna, int *pontosPreto, int *pontosBranco)
 {
+    /*
+       int linhaAdj, colunaAdj;
+       int linhaIncr, colunaIncr;
+       int encontrouOutro, acabou;
+
+       tabuleiro[linha][coluna] = jogadorAtual;
+
+       for (linhaIncr = -1; linhaIncr <= 1; linhaIncr++)
+       {
+           for (colunaIncr = -1; colunaIncr <= 1; colunaIncr++)
+           {
+               if (linhaIncr != 0 && colunaIncr != 0)
+               {
+                   linhaAdj = linha + linhaIncr;
+                   colunaAdj = coluna + colunaIncr;
+                   encontrouOutro = 0;
+                   acabou = 0;
+                   while (linhaAdj >= 0 && linhaAdj < TAMANHO_TABULEIRO && colunaAdj >= 0 && colunaAdj < TAMANHO_TABULEIRO && acabou == 0)
+                   {
+                       if (tabuleiro[linhaAdj][colunaAdj] == VAZIO)
+                       {
+                           acabou = 1;
+                       }
+                       else if (tabuleiro[linhaAdj][colunaAdj] == jogadorAtual)
+                       {
+                           if (encontrouOutro)
+                           {
+                               linhaAdj -= linhaIncr;
+                               colunaAdj -= colunaIncr;
+                               printf("----\n");
+                               printf("%d %d\n", linhaAdj, colunaAdj);
+                               printf("----\n");
+                               while (tabuleiro[linhaAdj][colunaAdj] != jogadorAtual)
+                               {
+                                   tabuleiro[linhaAdj][colunaAdj] = jogadorAtual;
+                                   linhaAdj -= linhaIncr;
+                                   colunaAdj -= colunaIncr;
+                               }
+                           }
+                           acabou = 1;
+                       }
+                       encontrouOutro = 1;
+                       linhaAdj += linhaIncr;
+                       colunaAdj += colunaIncr;
+                   }
+               }
+           }
+       }
+       */
+    int r, c;
+    int dr, dc;
+    int found_other;
+
     tabuleiro[linha][coluna] = jogadorAtual;
 
-    int linhaIncr, colunaIncr;
-    int linhaAdj, colunaAdj;
-    int encontrouAdvers = 0;
-
-    for (linhaIncr = -1; linhaIncr <= 1; linhaIncr++)
+    for (dr = -1; dr <= 1; dr++)
     {
-        for (colunaIncr = -1; colunaIncr <= 1; colunaIncr++)
+        for (dc = -1; dc <= 1; dc++)
         {
-            if (linhaIncr != 0 || colunaIncr != 0)
+            if (dr != 0 || dc != 0)
             {
-                linhaAdj = linha + linhaIncr;
-                colunaAdj = coluna + colunaIncr;
-
-                encontrouAdvers = 0;
-                int empty_found = 0;
-
-                while (linhaAdj >= 0 && linhaAdj < TAMANHO_TABULEIRO && colunaAdj >= 0 && colunaAdj < TAMANHO_TABULEIRO && !empty_found)
+                r = linha + dr;
+                c = coluna + dc;
+                found_other = 0;
+                while (r >= 0 && r < TAMANHO_TABULEIRO && c >= 0 && c < TAMANHO_TABULEIRO)
                 {
-                    /*if (tabuleiro[linhaAdj][colunaAdj] == Empty)
+                    if (tabuleiro[r][c] == VAZIO)
                     {
-                        empty_found = 1;
-                    }*/
-                    else if (tabuleiro[linhaAdj][colunaAdj] == jogadorAtual && encontrouAdvers)
+                        break;
+                    }
+                    if (tabuleiro[r][c] == jogadorAtual)
                     {
-                        int rr = row + linhaIncr;
-                        int cc = col + colunaIncr;
-                        while (rr != r || cc != c)
+                        if (found_other)
                         {
-                            tabuleiro[rr][cc] = current_player;
-                            rr += linhaIncr;
-                            cc += colunaIncr;
+                            r -= dr;
+                            c -= dc;
+                            while (tabuleiro[r][c] != jogadorAtual)
+                            {
+                                tabuleiro[r][c] = jogadorAtual;
+                                r -= dr;
+                                c -= dc;
+                            }
                         }
+                        break;
                     }
-                    else if (tabuleiro[linhaAdj][colunaAdj] != current_player)
-                    {
-                        encontrouAdvers = 1;
-                    }
-                    r += linhaIncr;
-                    c += colunaIncr;
+                    found_other = 1;
+                    r += dr;
+                    c += dc;
                 }
             }
         }
@@ -180,15 +280,24 @@ void jogar(int pecaJogador, int undoJogada, FILE *fp)
     JOGADA *top = NULL;
     JOGADA *novaJogada;
 
-    int *pontosPreto = 2, *pontosBranco = 2;
+    int pontosPreto = 2, pontosBranco = 2;
+
     int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO];
 
     inicializarTabuleiro(tabuleiro);
+    imprimirTabuleiro(tabuleiro);
 
     while (haJogadaValida(tabuleiro, jogadorAtual))
     {
-        imprimirTabuleiro(tabuleiro);
-        /* Impressao da linha especial printf("Current player: %s\n", (current_player == Black) ? "Black" : "White");*/
+        if (jogadorAtual)
+        {
+            printf("Jogador 1 (X) (linha e coluna) = ");
+        }
+        else
+        {
+            printf("Jogador 2 (O) (linha e coluna) = ");
+        }
+
         if (pecaJogador == jogadorAtual) /* Jogada do jogador */
         {
             int valid_move = 0;
@@ -196,26 +305,26 @@ void jogar(int pecaJogador, int undoJogada, FILE *fp)
             {
                 if (scanf("%d %d", &linha, &coluna) == 2)
                 {
-                    fazerJogada();
+                    /*fazerJogada();*/
+                    if (eJogadaValida(tabuleiro, jogadorAtual, linha, coluna))
+                    {
+                        fazerJogada(tabuleiro, jogadorAtual, linha, coluna, &pontosPreto, &pontosBranco);
+                        valid_move = 1;
+                    }
+                    else
+                    {
+                        printf("Jogada ilegal, porfavor tenta outra vez.\n");
+                    }
                 }
                 else
                 {
                     undo();
                 }
-
-                /*if (is_valid_move(row, col))
-                {
-                    valid_move = 1;
-                }
-                else
-                {
-                    printf("Jogada ilegal, porfavor tenta outra vez.\n");
-                }*/
             }
         }
         else /* Jogada da IA */
         {
-            int melhorValor = -1;
+            /*Pint melhorValor = -1;
             int valor;
             int melhorLinha, melhorColuna;
             int i, j;
@@ -237,10 +346,37 @@ void jogar(int pecaJogador, int undoJogada, FILE *fp)
             }
             linha = melhorLinha;
             coluna = melhorColuna;
+            printf("%d %d\n", linha, coluna);
+            */
+            int max_value = -1000;
+            int best_row, best_col;
+            int i, j;
+            for (i = 0; i < TAMANHO_TABULEIRO; i++)
+            {
+                for (j = 0; j < TAMANHO_TABULEIRO; j++)
+                {
+                    if (eJogadaValida(tabuleiro, jogadorAtual, i, j))
+                    {
+                        int value = avaliarPosicao(tabuleiro, jogadorAtual, i, j);
+                        if (value > max_value)
+                        {
+                            max_value = value;
+                            best_row = i;
+                            best_col = j;
+                        }
+                    }
+                }
+            }
+            linha = best_row;
+            coluna = best_col;
             printf("Computer played: %d %d\n", linha, coluna);
         }
+        jogadorAtual = mudarJogador(jogadorAtual);
 
-        fazerJogada(tabuleiro, linha, coluna, &pontosPreto, &pontosBranco);
+        printf("\n");
+        imprimirTabuleiro(tabuleiro);
+
+        printf("-------------------------------------\n\n");
 
         if (fp != NULL)
         {
